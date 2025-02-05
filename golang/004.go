297c8af01293febaba85217276e3eee2d39881a7
gonyango <grantononyango@gmail.com>
2025-02-05 15:29:52 +0300

    refactor: used a map to create unique indexes and appended to pointer

--- golang/004.go
+++ golang/004.go
@@ -3,20 +3,17 @@ package main
 import "fmt"
 
 func removeDuplicates(arr []int) []int{
-	start:= 0
-	stop := len(arr) - 1
-	pointer := arr[0]
-	for start <= stop{
-		if start == 0{
-			arr = append(arr, pointer)
+	seen := make(map[int]bool)
+	result := &[]int{}
+
+	for _,num := range arr{
+		if !seen[num]{
+			*result = append(*result, num)
+			seen[num] = true
 		}
-		if arr[start] != pointer{
-			arr = append(arr, arr[start])
-			pointer = arr[start]
-		}
-		start++
 	}
-	return arr[stop+1:]
+
+	return *result
 }
 
 func main() {
