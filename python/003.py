74122d87f4bfbfb99bea9cba71735cf4f3af45f6
gonyango <grantononyango@gmail.com>
2025-02-05 13:28:21 +0300

    removing redundant code

--- python/003.py
+++ python/003.py
@@ -1,13 +1,13 @@
 def isPalindrome(s):
     left, right = 0, len(s) - 1
     while left < right:
-        if not(isAlphanumeric(s[left])):
+        if not s[left].isalnum():
             left+=1
             continue
-        if not isAlphanumeric(s[right]):
+        if not s[right].isalnum():
             right-=1
             continue
-        if not isSame(s[left], s[right]):
+        if s[left].lower() != s[right].lower():
             return False
         left += 1
         right -= 1
@@ -20,4 +20,4 @@ def isAlphanumeric(char):
 def isSame(char1, char2):
     return char1.lower() == char2.lower()
 
-print(isPalindrome("0racecar0....."))
+print(isPalindrome("0race###Car0....."))
