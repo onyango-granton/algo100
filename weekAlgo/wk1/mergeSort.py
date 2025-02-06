f5606f85b98ee10fa49a1585c513ade227675801
gonyango <grantononyango@gmail.com>
2025-02-06 11:39:11 +0300

    added visualize function

--- weekAlgo/wk1/mergeSort.py
+++ weekAlgo/wk1/mergeSort.py
@@ -1,25 +1,26 @@
+import plotext as plt
+
 def divide(arr):
     if len(arr) < 2:
         return arr
-    midPoint = len(arr) / 2
+    midPoint = len(arr) // 2
 
     leftArr = arr[:midPoint]
     rightArr = arr[midPoint:]
 
-    return conquer(leftArr, rightArr)
+    return conquer(divide(leftArr), divide(rightArr))
 
 def conquer(leftArr, rightArr):
-    mainArr = []
+    mainArr = list(range(len(leftArr)+len(rightArr)))
     mainP, rightP, leftP = 0,0,0
     while rightP < len(rightArr) and leftP < len(leftArr):
         if rightArr[rightP] <= leftArr[leftP]:
             mainArr[mainP] = rightArr[rightP]
-            mainP += 1
             rightP += 1
         else:
             mainArr[mainP] = leftArr[leftP]
-            mainP += 1
             leftP += 1
+        mainP += 1
     while rightP < len(rightArr):
         mainArr[mainP] = rightArr[rightP]
         mainP += 1
@@ -29,5 +30,12 @@ def conquer(leftArr, rightArr):
         mainP += 1
         leftP += 1
     return mainArr
+
+def visualize_data(data):
+    plt.yticks(data.get("Y"))
+    plt.bar(data.get("X"))
+
+    plt.show()
+
 
 print(divide([3,6,7,3,2,5,6,57,4,34]))
