fe4b86a7fa6f8e63f2b0ae0945862939e82957a3
gonyango <grantononyango@gmail.com>
2025-02-07 15:57:45 +0300

    feat: function to implement task 5: ginding common elems in two arrays

--- python/005.py
+++ python/005.py
@@ -33,32 +33,31 @@ def findCommonElems(arrOne, arrTwo):
     onePointer, twoPointer = 0,0
     resultArr = []
 
-    print(len(resultArr))
+    # print(len(resultArr))
 
     if len(sortArrTwo) < len(sortArrOne):
-        copyArr = sortArrTwo
-        sortArrTwo = sortArrOne
-        sortArrOne = copyArr
+        sortArrOne, sortArrTwo = sortArrTwo, sortArrOne
 
-    while onePointer < len(sortArrOne):
+    while onePointer < len(sortArrOne) and twoPointer < len(sortArrTwo):
         if sortArrOne[onePointer] == sortArrTwo[twoPointer]:
+            # print(len(resultArr))
             if len(resultArr) == 0:
                 resultArr.append(sortArrOne[onePointer])
                 onePointer += 1
                 twoPointer += 1
             elif sortArrOne[onePointer] != resultArr[len(resultArr)-1]:
-                resultArr = resultArr.append(sortArrOne[onePointer])
+                resultArr.append(sortArrOne[onePointer])
                 onePointer += 1
                 twoPointer += 1
             else:
                 onePointer += 1
                 twoPointer += 1
+        elif sortArrOne[onePointer] > sortArrTwo[twoPointer]:
+            twoPointer += 1
         elif sortArrOne[onePointer] < sortArrTwo[twoPointer]:
             onePointer += 1
-        elif sortArrTwo[twoPointer] < sortArrOne[onePointer]:
-            twoPointer += 1
 
     return resultArr
 
-print(divide([6,3,2,8,9,2,5]))
-print(findCommonElems([4,5,6,7,3,23,5],[2,5,6,2,4,4,2]))
+# print(divide([6,3,2,8,9,2,5]))
+print(findCommonElems([4,5,6,7,3,23,5],[2,5,6,2,4,4,2,8]))
