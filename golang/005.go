1e0588ee13be4ba3095e0f9e988c9ff0708dc43c
gonyango <grantononyango@gmail.com>
2025-02-07 14:41:18 +0300

    improved code and removed redundsncy

--- golang/005.go
+++ golang/005.go
@@ -2,9 +2,9 @@ package main
 
 import "fmt"
 
-func divide(arr []int) []int{
+func divide(arr []int) []int {
 	//base case
-	if len(arr) < 2{
+	if len(arr) < 2 {
 		return arr
 	}
 
@@ -16,11 +16,11 @@ func divide(arr []int) []int{
 }
 
 func conquer(leftArr, rightArr []int) []int {
-	mainArr := make([]int, len(leftArr)+ len(rightArr))
-	mainP, leftP, rightP := 0,0,0
+	mainArr := make([]int, len(leftArr)+len(rightArr))
+	mainP, leftP, rightP := 0, 0, 0
 
-	for leftP < len(leftArr) && rightP < len(rightArr){
-		if leftArr[leftP]<rightArr[rightP]{
+	for leftP < len(leftArr) && rightP < len(rightArr) {
+		if leftArr[leftP] < rightArr[rightP] {
 			mainArr[mainP] = leftArr[leftP]
 			mainP++
 			leftP++
@@ -29,16 +29,16 @@ func conquer(leftArr, rightArr []int) []int {
 			mainP++
 			rightP++
 		}
-		
+
 	}
 
-	for leftP < len(leftArr){
+	for leftP < len(leftArr) {
 		mainArr[mainP] = leftArr[leftP]
 		mainP++
 		leftP++
 	}
 
-	for rightP < len(rightArr){
+	for rightP < len(rightArr) {
 		mainArr[mainP] = rightArr[rightP]
 		mainP++
 		rightP++
@@ -47,65 +47,45 @@ func conquer(leftArr, rightArr []int) []int {
 	return mainArr
 }
 
-func compareArr(arrOne, arrTwo []int) []int{
+func compareArr(arrOne, arrTwo []int) []int {
 	sortedArrOne, sortedArrTwo := divide(arrOne), divide(arrTwo)
-	pointerOne, pointerTwo := 0,0
+	pointerOne, pointerTwo := 0, 0
 	resArr := []int{}
 
+	if len(sortedArrTwo) < len(sortedArrOne) {
+		copyArr := sortedArrTwo
+		sortedArrTwo = sortedArrOne
+		sortedArrOne = copyArr
+	}
 
-	if len(sortedArrOne) < len(sortedArrTwo){
-	
-		for pointerOne < len(sortedArrOne){
-			if sortedArrOne[pointerOne] == sortedArrTwo[pointerTwo]{
-				if len(resArr) - 1 < 0{
-					resArr = append(resArr, sortedArrOne[pointerOne])
-					pointerOne++
-					pointerTwo++
-				} else if sortedArrOne[pointerOne] != resArr[len(resArr)-1]{
-					resArr = append(resArr, sortedArrOne[pointerOne])
-					pointerOne++
-					pointerTwo++
-				} else {
-					pointerOne++
-					pointerTwo++
-				}
-				
-			} else if sortedArrOne[pointerOne] > sortedArrTwo[pointerTwo]{
+	for pointerOne < len(sortedArrOne) {
+		if sortedArrOne[pointerOne] == sortedArrTwo[pointerTwo] {
+			if len(resArr)-1 < 0 {
+				resArr = append(resArr, sortedArrOne[pointerOne])
+				pointerOne++
 				pointerTwo++
-			} else if sortedArrOne[pointerOne] < sortedArrTwo[pointerTwo]{
+			} else if sortedArrOne[pointerOne] != resArr[len(resArr)-1] {
+				resArr = append(resArr, sortedArrOne[pointerOne])
 				pointerOne++
-			}
-		}
-	} else {
-		for pointerTwo < len(sortedArrTwo){
-			if sortedArrTwo[pointerTwo] == sortedArrOne[pointerOne]{
-				if len(resArr)-1 < 0 {
-					resArr = append(resArr, sortedArrTwo[pointerTwo])
-					pointerOne++
-					pointerTwo++
-				} else if sortedArrTwo[pointerTwo] != resArr[len(resArr)-1]{
-					resArr = append(resArr, sortedArrTwo[pointerTwo])
-					pointerOne++
-					pointerTwo++
-				} else {
-					pointerOne++
-					pointerTwo++
-				}
-				
-			} else if sortedArrTwo[pointerTwo] > sortedArrOne[pointerOne]{
+				pointerTwo++
+			} else {
 				pointerOne++
-			} else if sortedArrTwo[pointerTwo] < sortedArrOne[pointerOne]{
 				pointerTwo++
 			}
+
+		} else if sortedArrOne[pointerOne] > sortedArrTwo[pointerTwo] {
+			pointerTwo++
+		} else if sortedArrOne[pointerOne] < sortedArrTwo[pointerTwo] {
+			pointerOne++
 		}
 	}
 
 	return resArr
 }
 
-func main(){
+func main() {
 	nums1 := []int{5}
-	nums2 := []int{9, 4,4, 9, 8, 4,5}
+	nums2 := []int{9, 4, 4, 9, 8, 4, 5}
 	// fmt.Println(divide([]int{3,6,8,3,24,5,5,3,42,4,67,6}))
-	fmt.Println(compareArr(nums1,nums2))
+	fmt.Println(compareArr(nums2, nums1))
 }
