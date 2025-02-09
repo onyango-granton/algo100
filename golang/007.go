18004f5a8e5ac0d560a1669d132a8ac0be5feb99
gonyango <grantononyango@gmail.com>
2025-02-09 16:09:35 +0300

    feat: function to totate members of array by k elems, implemeted it using for loops big 0 n for time space

---
+++ golang/007.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func rotateArr(arr []int, positions int) []int {
+	realPointer := 0
+	copyArr := make([]int, len(arr))
+	copy(copyArr,arr)
+
+	fmt.Println(copyArr)
+
+	for i := positions +1; i < len(arr); i++ {
+		fmt.Print("here")
+		arr[realPointer] = arr[i]
+		realPointer++
+	}
+	fmt.Println(arr)
+
+
+
+	for i := 0; i <= positions; i++ {
+		arr[realPointer] = copyArr[i]
+		realPointer++
+	}
+
+	return arr
+}
+
+func main() {
+	fmt.Println(rotateArr([]int{1,2,3,4,5,6,7}, 3))
+}
