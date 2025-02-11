7088cf2f0d256d5ec57bcc7c1a42d1bd7d4c4ca2
gonyango <grantononyango@gmail.com>
2025-02-11 10:36:39 +0300

    rotateArr python function; with some changes

--- python/007.py
+++ python/007.py
@@ -1,16 +1,5 @@
 def rotateArr(arr, positions):
-    realPointer = 0
-    copyArr =  [x for x in arr]
-
-
-    for x in range (positions+1, len(arr)):
-        arr[realPointer] = arr[x]
-        realPointer+=1
-    
-    for x in range (0, positions+1):
-        arr[realPointer] = copyArr[x]
-        realPointer+=1
-
-    return arr
+#    positions = positions % len(arr)
+   return arr[positions+2:]+arr[:len(arr)-positions]
 
 print(rotateArr([3,4,5,6,7,8,9,10],3))
