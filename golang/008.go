fa5ecfe563bf96e3933a0b3aa0755a3dadc2406d
gonyango <grantononyango@gmail.com>
2025-02-11 15:41:19 +0300

    moved main function

--- golang/008.go
+++ golang/008.go
@@ -1,27 +1,24 @@
 package main
 
-import "fmt"
-
 func findNum(arr []int, target int, start, stop int) int {
-	if start  > stop {
+	if start > stop {
 		return -1
 	}
-	mid := start +(stop - start )/ 2
+	mid := start + (stop-start)/2
 
 	// fmt.Println(mid)
-	
 
-	if arr[mid] == target{
+	if arr[mid] == target {
 		return mid
 	}
 
-	if target > arr[mid]{
-		return findNum(arr, target,mid+1,stop)
+	if target > arr[mid] {
+		return findNum(arr, target, mid+1, stop)
 	} else {
-		return findNum(arr, target, start, mid-1 )
+		return findNum(arr, target, start, mid-1)
 	}
 }
 
-func main(){
-	fmt.Println(findNum([]int{2,3,5,6,7,8},10,0,5))
-}
+// func main(){
+// 	fmt.Println(findNum([]int{2,3,5,6,7,8},10,0,5))
+// }
