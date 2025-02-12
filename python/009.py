1e47acedf4f9653dbfd3aeb246520cc73a90dfe9
gonyango <grantononyango@gmail.com>
2025-02-12 11:44:04 +0300

    feat: lfunction to get first and last occurence of an element in an array in python using bubble sor... achieves (log n + m)

---
+++ python/009.py
@@ -0,0 +1,29 @@
+def findFirstAndLast(arr, target, first, last):
+    position = binarySort(arr,target, 0, len(arr)- 1)
+    positionCopy = position
+    while arr[position] == target:
+        first = position
+        position-=1
+    while arr[positionCopy] == target:
+        last = positionCopy
+        positionCopy+=1
+    return [first, last]
+
+def binarySort(arr, target, start,stop):
+    if start > stop:
+        return -1
+    
+    midVal = start + (stop-start) // 2
+
+    if target == arr[midVal]:
+        return midVal
+
+    elif target > arr[midVal]:
+        return binarySort(arr, target, midVal + 1, stop)
+
+    else:
+        return binarySort(arr, target, start, midVal - 1)
+    
+
+print(binarySort([2,3,4,5,6,7,8],8,0,6))
+print(findFirstAndLast([3,4,5,5,5,6,7,8],5,0,0))
