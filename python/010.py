b8e9c8f3a917d4a5487ed4da765759069200ffb2
gonyango <grantononyango@gmail.com>
2025-02-12 14:52:31 +0300

    recurse solve bubble sort

--- python/010.py
+++ python/010.py
@@ -5,4 +5,16 @@ def bubblesort(arr):
                 arr[x], arr[x+1] = arr[x+1], arr[x]
     return arr
 
-print(bubblesort([2,3,5,6,7,2,3,4,2,4,453,5,4]))
+def recurseBubbleSort(arr, iteration, pointer):
+    if pointer == len(arr):
+        pointer = 0
+        iteration+=1
+
+    if iteration == len(arr):
+        return arr
+    
+    if pointer + 1 < len(arr) and arr[pointer] > arr[pointer + 1]:
+        arr[pointer], arr[pointer + 1] = arr[pointer + 1], arr[pointer]
+    return recurseBubbleSort(arr, iteration, pointer+1)
+
+print(recurseBubbleSort([2,3,5,6,7,2,3,4,2,4,453,5,4],0,0))
