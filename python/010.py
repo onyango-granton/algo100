c7570a0fad7730c2120347a7cbf9bf10081dd5b1
gonyango <grantononyango@gmail.com>
2025-02-12 15:10:55 +0300

    breaks the for loop earlier once swapping has stopped

--- python/010.py
+++ python/010.py
@@ -1,8 +1,12 @@
 def bubblesort(arr):
     for x in range(0,len(arr)):
+        swapped =  False
         for x in range(0,len(arr)):
             if x + 1 < len(arr) and arr[x] > arr[x+1]:
                 arr[x], arr[x+1] = arr[x+1], arr[x]
+                swapped = True
+        if not swapped:
+            break
     return arr
 
 def recurseBubbleSort(arr, iteration, pointer):
@@ -17,4 +21,4 @@ def recurseBubbleSort(arr, iteration, pointer):
         arr[pointer], arr[pointer + 1] = arr[pointer + 1], arr[pointer]
     return recurseBubbleSort(arr, iteration, pointer+1)
 
-print(recurseBubbleSort([2,3,5,6,7,2,3,4,2,4,453,5,4],0,0))
+print(bubblesort([2,3,5,6,7,2,3,4,2,4,453,5,4]))
