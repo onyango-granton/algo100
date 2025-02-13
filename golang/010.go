0bc3d90ab59b405dd2ecd576cd19d72a5b4d8170
gonyango <grantononyango@gmail.com>
2025-02-13 10:51:06 +0300

    refactor moved main function

--- golang/010.go
+++ golang/010.go
@@ -1,36 +1,35 @@
 package main
 
-import "fmt"
-
-func bubbleSort(arr []int)[]int{
-	for range arr{
-		for i := range arr{
-		if i+1 < len(arr){
-			if arr[i] > arr[i+1]{
-				arr[i], arr[i+1] = arr[i+1], arr[i]
+func bubbleSort(arr []int) []int {
+	for range arr {
+		for i := range arr {
+			if i+1 < len(arr) {
+				if arr[i] > arr[i+1] {
+					arr[i], arr[i+1] = arr[i+1], arr[i]
+				}
 			}
 		}
-	}}
+	}
 
 	return arr
 }
 
 func recursiveCallBubbleSort(arr []int, pointer, iteration int) []int {
-	//pointer reset
-	if pointer == len(arr){
+	// pointer reset
+	if pointer == len(arr) {
 		pointer = 0
 		iteration++
 	}
-	//base case
-	if iteration == len(arr){
+	// base case
+	if iteration == len(arr) {
 		return arr
 	}
-	if pointer + 1 < len(arr) && arr[pointer] > arr[pointer + 1]{
-		arr[pointer], arr[pointer + 1] = arr[pointer + 1], arr[pointer]
+	if pointer+1 < len(arr) && arr[pointer] > arr[pointer+1] {
+		arr[pointer], arr[pointer+1] = arr[pointer+1], arr[pointer]
 	}
 	return recursiveCallBubbleSort(arr, pointer+1, iteration)
 }
 
-func main(){
-	fmt.Println(recursiveCallBubbleSort([]int{3,4,5,6,7,2,223,4,4,2},0,0))
-}
+// func main(){
+// 	fmt.Println(recursiveCallBubbleSort([]int{3,4,5,6,7,2,223,4,4,2},0,0))
+// }
