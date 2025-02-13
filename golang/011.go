940613009a1549cc8391b33a3d42879df601e29d
gonyango <grantononyango@gmail.com>
2025-02-13 16:26:20 +0300

    moved main function

--- golang/011.go
+++ golang/011.go
@@ -1,21 +1,18 @@
 package main
 
-import "fmt"
-
-func insertionSort(arr []int)[]int{
-
-	for i := range arr{
+func insertionSort(arr []int) []int {
+	for i := range arr {
 		k := i
-		for k > 0 && arr[k-1] > arr[k]{
+		for k > 0 && arr[k-1] > arr[k] {
 			arr[k-1], arr[k] = arr[k], arr[k-1]
 			k--
-		} 
+		}
 
 	}
 	return arr
 }
 
-func main(){
-	// fmt.Println(insertionSort([]int{2,3,2,4,5,6,3,4,2,5}))
-	fmt.Println(insertionSort([]int{4,2,5,6,6,23,23,3,42,6}))
-}
+// func main(){
+// 	// fmt.Println(insertionSort([]int{2,3,2,4,5,6,3,4,2,5}))
+// 	fmt.Println(insertionSort([]int{4,2,5,6,6,23,23,3,42,6}))
+// }
