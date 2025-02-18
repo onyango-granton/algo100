2f19cafab13156282f88320d844dab4194b7f7b9
gonyango <grantononyango@gmail.com>
2025-02-18 12:36:22 +0300

    moved main function

--- golang/014.go
+++ golang/014.go
@@ -1,19 +1,17 @@
 package main
 
-import "fmt"
-
 func getUnion(arr1, arr2 []int) []int {
 	numMap := make(map[int]bool)
 	var resArr []int
 
-	for _,num := range arr1{ 
-		if !numMap[num] { 
+	for _, num := range arr1 {
+		if !numMap[num] {
 			resArr = append(resArr, num)
 			numMap[num] = true
 		}
 	}
 
-	for _,num := range arr2{
+	for _, num := range arr2 {
 		if !numMap[num] {
 			resArr = append(resArr, num)
 			numMap[num] = true
@@ -23,8 +21,8 @@ func getUnion(arr1, arr2 []int) []int {
 	return resArr
 }
 
-func main(){
-	arr1 := []int{2,3,5,6,7,4,5,6,2,5} 
-	arr2 := []int{3,4,5,1,4,5,3,5,3}
-	fmt.Println(getUnion(arr1,arr2))
-}
+// func main(){
+// 	arr1 := []int{2,3,5,6,7,4,5,6,2,5}
+// 	arr2 := []int{3,4,5,1,4,5,3,5,3}
+// 	fmt.Println(getUnion(arr1,arr2))
+// }
