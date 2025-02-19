6d4cb3336f746fff69fc62e84a1c093f5db07c4d
gonyango <grantononyango@gmail.com>
2025-02-19 15:02:18 +0300

    task 16 finding first occurence of a non-repeating char in a word; time(n) space(n)

--- python/016.py
+++ python/016.py
@@ -1,17 +1,19 @@
 def findFirstNonRepeatingChar(word):
-    charDic = {}
+    char_Dict = {}
     index = len(word)
-    for x in range(len(word)):
-        charDic.setdefault(word[x],[]).append(x)
+    
+    for i, char in enumerate(word):
+        char_Dict.setdefault(char, []).append(i)
 
-    for key in charDic:
-        if len(charDic[key]) == 1:
-            if charDic[key][0] < index:
-                index = charDic[key][0]
+    for key in char_Dict:
+        if len(char_Dict[key]) == 1:
+            if char_Dict[key][0] < index:
+                index = char_Dict[key][0]
 
     if index >= 0 and index < len(word):
         return index
     else:
         return -1
 
-print(findFirstNonRepeatingChar("wordw"))
+
+print(findFirstNonRepeatingChar("ww"))
