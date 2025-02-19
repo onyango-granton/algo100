ce56aa69436cf8f7d8137f383b3eb87fbb049867
gonyango <grantononyango@gmail.com>
2025-02-19 16:47:32 +0300

    moved main function

--- golang/016.go
+++ golang/016.go
@@ -1,24 +1,22 @@
 package main
 
-import "fmt"
-
 func firstNonRepeatingChar(word string) int {
 	charMap := make(map[string][]int)
 	min := len(word)
 
-	for i, ch := range word{
+	for i, ch := range word {
 		charMap[string(ch)] = append(charMap[string(ch)], i)
 	}
 
-	for _, value := range charMap{
-		if len(value) == 1{
-			if value[0] < min{
+	for _, value := range charMap {
+		if len(value) == 1 {
+			if value[0] < min {
 				min = value[0]
 			}
 		}
 	}
 
-	if min >= 0 && min <= len(word)-1{
+	if min >= 0 && min <= len(word)-1 {
 		return min
 	} else {
 		return -1
@@ -27,6 +25,6 @@ func firstNonRepeatingChar(word string) int {
 	// return min
 }
 
-func main(){
-	fmt.Println(firstNonRepeatingChar("aabb"))
-}
+// func main(){
+// 	fmt.Println(firstNonRepeatingChar("aabb"))
+// }
