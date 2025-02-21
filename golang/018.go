cf6d5907713a8311f0649e1b247005813cf8c097
gonyango <grantononyango@gmail.com>
2025-02-21 10:51:07 +0300

    tak 18 two sum function to find index of two numbers who add up to a target

--- golang/018.go
+++ golang/018.go
@@ -2,58 +2,69 @@ package main
 
 import "fmt"
 
-// func mergeSort(arr []int) []int {
-// 	if len(arr) < 2 {
-// 		return arr
-// 	}
-// 	mid := len(arr) / 2
+func mergeSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+	mid := len(arr) / 2
 
-// 	return merge(mergeSort(arr[:mid]), mergeSort(arr[mid:]))
-// }
+	return merge(mergeSort(arr[:mid]), mergeSort(arr[mid:]))
+}
 
-// func merge(leftArr, rightArr []int) []int {
-// 	mainArr := make([]int, len(leftArr)+len(rightArr))
-// 	lpointer, rpointer, mpointer := 0, 0, 0
+func merge(leftArr, rightArr []int) []int {
+	mainArr := make([]int, len(leftArr)+len(rightArr))
+	lpointer, rpointer, mpointer := 0, 0, 0
 
-// 	for lpointer < len(leftArr) && rpointer < len(rightArr) {
-// 		if leftArr[lpointer] < rightArr[rpointer] {
-// 			mainArr[mpointer] = leftArr[lpointer]
-// 			lpointer++
-// 		} else {
-// 			mainArr[mpointer] = rightArr[rpointer]
-// 			rpointer++
-// 		}
-// 		mpointer++
-// 	}
+	for lpointer < len(leftArr) && rpointer < len(rightArr) {
+		if leftArr[lpointer] < rightArr[rpointer] {
+			mainArr[mpointer] = leftArr[lpointer]
+			lpointer++
+		} else {
+			mainArr[mpointer] = rightArr[rpointer]
+			rpointer++
+		}
+		mpointer++
+	}
 
-// 	for lpointer < len(leftArr) {
-// 		mainArr[mpointer] = leftArr[lpointer]
-// 		mpointer++
-// 		lpointer++
-// 	}
+	for lpointer < len(leftArr) {
+		mainArr[mpointer] = leftArr[lpointer]
+		mpointer++
+		lpointer++
+	}
 
-// 	for rpointer < len(rightArr) {
-// 		mainArr[mpointer] = rightArr[rpointer]
-// 		mpointer++
-// 		rpointer++
-// 	}
+	for rpointer < len(rightArr) {
+		mainArr[mpointer] = rightArr[rpointer]
+		mpointer++
+		rpointer++
+	}
 
-// 	return mainArr
-// }
+	return mainArr
+}
 
 func twoSum(arr []int, target int) [][]int {
 	arr = mergeSort(arr)
 	indexes := [][]int{}
 
+	intMap := make(map[int]bool)
+
+	for i:= range arr{
+		intMap[i] = false
+	}
+
 	lpointer, rpointer := 0, len(arr)-1
 
 	for lpointer < rpointer{
 		if arr[lpointer] + arr[rpointer] == target{
-			indexes = append(indexes, []int{lpointer,rpointer})
+			if !intMap[lpointer] && !intMap[rpointer]{
+				indexes = append(indexes, []int{lpointer,rpointer})
+				intMap[lpointer] = true
+				intMap[rpointer] = true
+			}
+			
 			lpointer++
 			rpointer--
 		} else if arr[lpointer] + arr[rpointer] > target{
-			if arr[lpointer] > target{
+			if arr[lpointer] > target && lpointer-1 >= 0{
 				lpointer--
 			} else {
 				rpointer--
