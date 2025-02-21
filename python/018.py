7d5067143fe415931bbd08363282e2036d5fc9e0
gonyango <grantononyango@gmail.com>
2025-02-21 14:14:56 +0300

    task 18 two sum implememtation to get two indexes fo numbers that add up to a target

--- python/018.py
+++ python/018.py
@@ -12,11 +12,36 @@ def quicksort(arr):
 def twoSum(arr,target):
     arr = quicksort(arr)
 
-    l,r = 0,0
+    l,r = 0,len(arr)-1
+
+    intDict = {}
+
+    sumList = [[]]
+
+    for x in range(len(arr)):
+        intDict[x] = False
 
     while l < r:
         if arr[l] + arr[r] == target:
+            if not intDict[l] and not intDict[r]:
+                sumList.append([l,r])
+                intDict[l] = True
+                intDict[r] = True
+            l += 1
+            r -= 1
+        elif arr[l] + arr[r] > target:
+            if arr[l] > target and l - 1 >= 0:
+                l -= 1
+            else:
+                r -= 1
+        elif arr[l] + arr[r] < target:
+            if arr[r] < target and r + 1 < len(arr):
+                r += 1
+            else:
+                l += 1
             
+    return sumList
 
 
-print(quicksort([2, 3, 2, 5, 7, 8, 5, 9, 6]))
+
+print(twoSum([2, 3, 2, 5, 7, 8, 5, 9, 6],9))
