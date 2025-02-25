0e0d584308d9f68852e81646ef7bd0137798e5ad
gonyango <grantononyango@gmail.com>
2025-02-25 12:18:33 +0300

    task 19 function finding a pair whose sum up to a target in golang

---
+++ golang/019.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func findPairWithSum(arr []int, target int) []int{
+	intMap := make(map[int]int)
+	intList := []int{}
+	indexList := []int{}
+
+	for i,ch := range arr{
+		intMap[ch] = target - arr[i]
+	}
+
+	for k,v := range intMap{
+		if intMap[v] != 0{
+			intList = append(intList, []int{k,v}...)
+			break
+		}
+	}
+
+	for i:=0; i < 2;{
+		for k:=0; k < len(arr);k++{
+			if intList[i] == arr[k]{
+				indexList = append(indexList, k)
+				i++
+			}
+			if i == 2{
+				break
+			}
+		}
+	}
+
+	return indexList
+}
+
+func main() {
+	arr := []int{2,3,4,5,6,6}
+	target := 9
+	// intMap := make(map[int]int)
+
+	fmt.Println(findPairWithSum(arr, target))
+}
