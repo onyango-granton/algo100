6f8a2f67aafc084b6fb99170ebdcab838a38aa94
gonyango <grantononyango@gmail.com>
2025-02-25 16:26:57 +0300

    moved main function

--- golang/019.go
+++ golang/019.go
@@ -1,30 +1,28 @@
 package main
 
-import "fmt"
-
-func findPairWithSum(arr []int, target int) []int{
+func findPairWithSum(arr []int, target int) []int {
 	intMap := make(map[int]int)
 	intList := []int{}
 	indexList := []int{}
 
-	for i,ch := range arr{
+	for i, ch := range arr {
 		intMap[ch] = target - arr[i]
 	}
 
-	for k,v := range intMap{
-		if intMap[v] != 0{
-			intList = append(intList, []int{k,v}...)
+	for k, v := range intMap {
+		if intMap[v] != 0 {
+			intList = append(intList, []int{k, v}...)
 			break
 		}
 	}
 
-	for i:=0; i < 2;{
-		for k:=0; k < len(arr);k++{
-			if intList[i] == arr[k]{
+	for i := 0; i < 2; {
+		for k := 0; k < len(arr); k++ {
+			if intList[i] == arr[k] {
 				indexList = append(indexList, k)
 				i++
 			}
-			if i == 2{
+			if i == 2 {
 				break
 			}
 		}
@@ -33,10 +31,10 @@ func findPairWithSum(arr []int, target int) []int{
 	return indexList
 }
 
-func main() {
-	arr := []int{2,3,4,5,6,6}
-	target := 9
-	// intMap := make(map[int]int)
+// func main() {
+// 	arr := []int{2,3,4,5,6,6}
+// 	target := 9
+// 	// intMap := make(map[int]int)
 
-	fmt.Println(findPairWithSum(arr, target))
-}
+// 	fmt.Println(findPairWithSum(arr, target))
+// }
