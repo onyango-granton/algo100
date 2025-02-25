6dcbb01f83e930e7bc4c87c5e025b501479af409
gonyango <grantononyango@gmail.com>
2025-02-25 16:06:52 +0300

    task 19 comments

--- python/019.py
+++ python/019.py
@@ -2,9 +2,12 @@ def findPairWithSum(arr, target):
     intMap = {}
 
     for index, value in enumerate(arr):
+        # calculating the complement for every n element
         complement = target - value
+        #  checking for the precense of complement elem in dictionary
         if complement in intMap:
             return [intMap[complement], index]
+        # adding each value and index to dictionary
         intMap[value] = index
 
     return []
