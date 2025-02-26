ac6c6ee2f9f8bfb26d22a378135bebefa29f7337
gonyango <grantononyango@gmail.com>
2025-02-26 13:48:59 +0300

    task 20 function to get subarray that sums upto 0

--- golang/020.go
+++ golang/020.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func zeroSumSubArr(arr []int) []int{
 	intMap := make(map[int]int)
 
@@ -16,4 +18,33 @@ func zeroSumSubArr(arr []int) []int{
 	}
 
 	return []int{}
+}
+
+func main(){
+	// intList := []int{1,2,-3,3,1,-4,2}
+	intList := []int{6, -1, -3, 4, -2, 2, 4, 6, -12, -7}
+
+	start := 0
+	stop := start
+
+	subArrList := [][]int{}
+
+	sum := 0
+
+	for stop < len(intList){
+		if sum + intList[stop] > sum && start > 0{
+			start = stop
+			sum = 0
+		} else {
+			sum += intList[stop]
+			if sum == 0 {
+				subArrList = append(subArrList, []int{start,stop})
+				start = stop + 1	
+			}
+		}
+		
+		stop ++
+	}
+
+	fmt.Println(subArrList)
 }
