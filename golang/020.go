10e01ba50a882ff7df227f3593e19a9228ae7038
gonyango <grantononyango@gmail.com>
2025-03-01 19:19:57 +0300

    task 20 function to find sub array with sum of zero bigo n2 golang

--- golang/020.go
+++ golang/020.go
@@ -28,15 +28,7 @@ func zeroSumSubArr(arr []int) [][]int {
 	return subs
 }
 
-func abs(num int) int {
-	if num < 0 {
-		return num * -1
-	}
-	return num
-}
-
 func main() {
-	// intList := []int{1,2,-3,3,1,-4,2}
 	intList := []int{6, -1, -3, 4, -2, 2, 4, 6, -12, -7}
 
 	fmt.Println(zeroSumSubArr(intList))
