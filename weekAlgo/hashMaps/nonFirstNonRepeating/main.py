0bb20e979154b61b4bb6fc4e3ff8adca17d00e51
gonyango <grantononyango@gmail.com>
2025-03-02 17:21:36 +0300

    chose to implement it in python... sorted em bugs"

--- weekAlgo/hashMaps/nonFirstNonRepeating/main.py
+++ weekAlgo/hashMaps/nonFirstNonRepeating/main.py
@@ -4,13 +4,15 @@ def firstNonRepeatingChar(word):
     firstOccurence = len(word)
 
     for index ,char in enumerate(word):
+        if char not in totalCharOccurence:
+            totalCharOccurence[char] = 0
         totalCharOccurence[char] += 1
         if char not in firstCharOccurence:
             firstCharOccurence[char] = index
 
-    for char, occurence in enumerate(firstCharOccurence):
-        if totalCharOccurence[char] == 1 and occurence < firstOccurence:
-            firstOccurence = occurence
+    for char in firstCharOccurence:
+        if totalCharOccurence[char] == 1 and firstCharOccurence[char] < firstOccurence:
+            firstOccurence = firstCharOccurence[char]
 
     if firstOccurence == len(word):
         return ""
