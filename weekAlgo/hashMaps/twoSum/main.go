10878dd118fa28a50f05dee355d1e4af0084910f
gonyango <grantononyango@gmail.com>
2025-03-02 15:49:20 +0300

    refactor: cleaned code following review; removing unnecessary storage, returnin nil value

--- weekAlgo/hashMaps/twoSum/main.go
+++ weekAlgo/hashMaps/twoSum/main.go
@@ -3,24 +3,19 @@ package main
 import "fmt"
 
 func twoSum(arr []int, target int) []int {
-	//indices := []int{}
-	//map current iteration index to (target - arr[i])
 	diffMap := make(map[int]int)
 
 	for i, num := range arr {
-		diff := target - arr[i]
-
 		val, found := diffMap[num]
 
-		//checks for precence of current arr index as key in map
 		if found {
 			return []int{val, i}
 		}
 
-		diffMap[diff] = i
+		diffMap[target-num] = i
 	}
 
-	return []int{}
+	return nil
 }
 
 func main() {
