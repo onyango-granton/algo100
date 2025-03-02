af980523fca94ce35c7f3b18e24396bf86da7618
gonyango <grantononyango@gmail.com>
2025-03-02 13:50:31 +0300

    task 20 code review implemetation find subarray that sum upto zero, with time space of n

--- golang/020.go
+++ golang/020.go
@@ -1,3 +1,28 @@
 package main
 
-func main
+import "fmt"
+
+func findSubArrayZero(arr []int) [][]int{
+	var sum int
+	subArr := [][]int{}
+	zeroSum := make(map[int][]int)
+	for i, num := range arr {
+		sum += num
+
+		vals, found := zeroSum[sum]
+
+		if found {
+			for _, value := range vals {
+				subArr = append(subArr, []int{value + 1, i + 1})
+			}
+		}
+
+		zeroSum[sum] = append(zeroSum[sum], i)
+	}
+
+	return subArr
+}
+
+func main() {
+	fmt.Println(findSubArrayZero([]int{6, -1, -3, 4, -2, 2, 4, 6, -12, -7}))
+}
