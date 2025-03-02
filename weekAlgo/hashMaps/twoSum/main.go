ea28f9cab9dc01c7a13c8fc2fe580cc1fc95c9db
gonyango <grantononyango@gmail.com>
2025-03-02 15:43:10 +0300

    additional comments to twoSum problem

--- weekAlgo/hashMaps/twoSum/main.go
+++ weekAlgo/hashMaps/twoSum/main.go
@@ -4,25 +4,25 @@ import "fmt"
 
 func twoSum(arr []int, target int) []int {
 	//indices := []int{}
+	//map current iteration index to (target - arr[i])
 	diffMap := make(map[int]int)
 
-	for i, num := range arr{
+	for i, num := range arr {
 		diff := target - arr[i]
 
 		val, found := diffMap[num]
 
-		if found{
-			return []int{val, i} 
+		//checks for precence of current arr index as key in map
+		if found {
+			return []int{val, i}
 		}
 
 		diffMap[diff] = i
 	}
 
-
-
 	return []int{}
 }
 
 func main() {
-	fmt.Println(twoSum([]int{2, 7, 11, 15},9))
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
 }
