f6c43bb90ca7811865efb7353cbba36eb690aa6b
gonyango <grantononyango@gmail.com>
2025-03-02 13:56:29 +0300

    task 20 code comments it took me some time to figure out what was going on but it'll take you less, cover how to use hashMaps from bigO of n2 to n

--- golang/020.go
+++ golang/020.go
@@ -4,13 +4,21 @@ import "fmt"
 
 func findSubArrayZero(arr []int) [][]int{
 	var sum int
+	//initalize subarray array 
 	subArr := [][]int{}
+
+	//hash map that will map list of indeces whose sum are same
+	//think of it as a wave each time it two trougs or crest are same in amplitude
+	//sums to zero
 	zeroSum := make(map[int][]int)
+
 	for i, num := range arr {
 		sum += num
 
 		vals, found := zeroSum[sum]
-
+		//if key already exsists
+		// itearte through the values obtaining sub arr with start point at values plus one 
+		// and endpoint at index + 1
 		if found {
 			for _, value := range vals {
 				subArr = append(subArr, []int{value + 1, i + 1})
@@ -23,6 +31,8 @@ func findSubArrayZero(arr []int) [][]int{
 	return subArr
 }
 
-func main() {
-	fmt.Println(findSubArrayZero([]int{6, -1, -3, 4, -2, 2, 4, 6, -12, -7}))
-}
+// for a practical env approach use this arr
+
+//func main() {
+//	fmt.Println(findSubArrayZero([]int{6, -1, -3, 4, -2, 2, 4, 6, -12, -7}))
+//}
