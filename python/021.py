7e299bd2b10a9034fd2eb9362fd7dd43313fd853
gonyango <grantononyango@gmail.com>
2025-03-03 10:16:23 +0300

    task 21 short hand for factorial function... cool :D

--- python/021.py
+++ python/021.py
@@ -1,6 +1,3 @@
-def factorial(num):
-    if num == 0:
-        return 1
-    return num * factorial(num - 1)
+def factorial(num): return 1 if num == 0 else num * factorial(num-1)
 
 print(factorial(5))
