7190fd1ab0fd0a0a6a224b91d910679397a7d999
gonyango <grantononyango@gmail.com>
2025-03-03 10:54:15 +0300

    task 22 fibonnaci now using memoization from O(n2) to O(n)

--- golang/022.go
+++ golang/022.go
@@ -2,11 +2,27 @@ package main
 
 import "fmt"
 
+// 1. Write a normal recursive function
+// 2. Create a map function for x imma get y
+// 3. Store x and y to the map
+// 4. return y for each time you have x
+
+var memo = make(map[int]int)
+
 func fibonacci(position int) int {
+	// base case
 	if position < 2{
 		return position
 	}
-	return fibonacci(position - 1) + fibonacci(position - 2)
+	// memo case
+	if val, found := memo[position]; found{
+		return val
+	}
+	// mapping values
+	memo[position] = fibonacci(position - 1) + fibonacci(position - 2)
+
+	// return value for each loop
+	return memo[position]
 }
 
 func main(){
