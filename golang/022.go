23736feb29de82d0ef0274a128bebe07956337fd
gonyango <grantononyango@gmail.com>
2025-03-03 10:37:30 +0300

    task 22 finding fibonacci num at a given position time O(n2)

--- golang/022.go
+++ golang/022.go
@@ -3,15 +3,12 @@ package main
 import "fmt"
 
 func fibonacci(num int) int {
-	if num == 0{
-		return 1
-	}
-	if num == 1{
-		return 1
+	if num < 2{
+		return num
 	}
 	return fibonacci(num - 1) + fibonacci(num - 2)
 }
 
 func main(){
-	fmt.Println(fibonacci(5))
+	fmt.Println(fibonacci(6))
 }
