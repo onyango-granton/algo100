a100b6c5e00ffd1c4d37e115a1814afec86217e5
gonyango <grantononyango@gmail.com>
2025-03-03 10:21:02 +0300

    refactor: moved main function

--- golang/021.go
+++ golang/021.go
@@ -1,12 +1,10 @@
 package main
 
-import "fmt"
-
 func factorial(num int) int {
-	if num == 0{
+	if num == 0 {
 		return 1
 	}
-	return num * factorial(num - 1)
+	return num * factorial(num-1)
 }
 
 // func main() {
