b0aa94b6e251eed8006c9e374512ec7a425de677
gonyango <grantononyango@gmail.com>
2025-03-04 17:39:06 +0300

    brief intro on decorators, a function taking in a funciton as an arg and returning another function as a result without modyfyin the main function

---
+++ weekAlgo/decorators/main.py
@@ -0,0 +1,26 @@
+def decoratorFunc(f):
+    def wrapper():
+        odd,even = f
+
+        for x in odd:
+            print(x, "Odd")
+        
+        for y in even:
+            print(y, "even")
+
+    return wrapper
+
+@decoratorFunc
+def oddeven(num, odd, even): 
+    if num > 100:
+        return  
+    if num % 2 == 0:
+        even.append(num)
+    else:
+        odd.append(num)
+    if num == 100:
+        return even, odd
+    
+    return oddeven(num+1, odd, even)
+
+oddeven(0, [], [])
