487132e6188dcdbcd7cc66016a842c6f09857f1c
gonyango <grantononyango@gmail.com>
2025-03-05 15:40:36 +0300

    moved main function

--- golang/022.go
+++ golang/022.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 // 1. Write a normal recursive function
 // 2. Create a map function for x imma get y
 // 3. Store x and y to the map
@@ -11,20 +9,20 @@ var memo = make(map[int]int)
 
 func fibonacci(position int) int {
 	// base case
-	if position < 2{
+	if position < 2 {
 		return position
 	}
 	// memo case
-	if val, found := memo[position]; found{
+	if val, found := memo[position]; found {
 		return val
 	}
 	// mapping values
-	memo[position] = fibonacci(position - 1) + fibonacci(position - 2)
+	memo[position] = fibonacci(position-1) + fibonacci(position-2)
 
 	// return value for each loop
 	return memo[position]
 }
 
-func main(){
-	fmt.Println(fibonacci(6))
-}
+// func main(){
+// 	fmt.Println(fibonacci(6))
+// }
