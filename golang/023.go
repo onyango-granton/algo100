f8c834a539683e6137d16dbbacc7131cbb9c4dcf
gonyango <grantononyango@gmail.com>
2025-03-06 14:47:32 +0300

    task 23; tower of hanoi function

--- golang/023.go
+++ golang/023.go
@@ -6,6 +6,7 @@ func tower_of_hanoi(n int, source, auxiliary, destination string){
 	if n == 0{
 		return
 	}
+	fmt.Println(n)
 	tower_of_hanoi(n-1,source, destination, auxiliary)
 	fmt.Println("Moved",n,"from",source,"to",destination)
 	tower_of_hanoi(n-1,auxiliary,source,destination)
