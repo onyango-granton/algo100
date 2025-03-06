523ef6fc3b7963edb15ab1bfae61715bc21e2be7
gonyango <grantononyango@gmail.com>
2025-03-06 14:42:33 +0300

    added comments to tower of hanoi gunction

--- python/023.py
+++ python/023.py
@@ -2,6 +2,7 @@ def tower_of_hanoi(n, source, auxiliary, destination):
     if n == 0:
         return
     
+    # auxiliary somewhat of the waiting,bay
     tower_of_hanoi(n-1, source, destination, auxiliary)
     print("Move", n, "from ",source, "to" ,destination)
     tower_of_hanoi(n-1, auxiliary, source, destination)
