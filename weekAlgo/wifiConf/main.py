01da8b75eab01c179e74eb7a1f366a9b59269aef
gonyango <grantononyango@gmail.com>
2025-03-08 13:18:47 +0300

    edited a function in wifi module to obtain available ssids i windows machine

--- weekAlgo/wifiConf/main.py
+++ weekAlgo/wifiConf/main.py
@@ -1,5 +1,47 @@
 import wifi
 import time
+import subprocess
 
-wifi_scanner = wifi.Cell.all("wlan")
-available_networks = [print(cell.ssid) for cell in wifi_scanner]
+def allWindows(cls, interface):
+        """
+        Returns a list of all cells extracted from the output of iwlist.
+        """
+        #try:
+            #iwlist_scan = subprocess.check_output(["netsh", interface, "show","network"])
+        #except subprocess.CalledProcessError as e:
+        #    raise InterfaceError(e.output.strip())
+        #else:
+        #    iwlist_scan = iwlist_scan.decode('utf-8')
+        cells = map(Cell.from_string, cells_re.split(iwlist_scan)[1:])
+
+        return cells
+
+#available_networks = wifi.Cell.allWindows("wlan")
+wifi_networks = wifi.Cell.allWindows("wlan")
+
+#available_networks = subprocess.check_output(["netsh", "wlan","show","network"])
+#available_networks = available_networks.decode('utf-8')
+
+print(wifi_networks)
+
+
+byteList = list(wifi_networks)
+
+newSentence = ""
+newList = list()
+
+for char in byteList:
+    if char == "\n":
+        newList.append(newSentence)
+        newSentence = ""
+        continue
+    if char == "\r":
+        continue
+    else:
+        newSentence += char
+#for sentence in newList:
+#     if "SSID" in sentence:
+#        print(sentence)
+
+print(*[sentence for sentence in newList if "SSID" in sentence], sep="\n")
+print(wifi_networks)
